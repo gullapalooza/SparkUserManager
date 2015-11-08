a2db00b2d31212f50ad02f5759bb681f0076951a
Anthony Santos <anthony.a.santos@gmail.com>
2015-11-08 12:44:19 -0800

    Created BlogPost service

--- src/main/java/io/anthonysantos/BlogService.java
+++ src/main/java/io/anthonysantos/BlogService.java
@@ -1,9 +1,105 @@
 package io.anthonysantos;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import lombok.Data;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BlogService {
+
+    private static final int HTTP_BAD_REQUEST = 400;
+
+    @Data
+    static class NewPostPayload {
+        private String title;
+        private List categories;
+        private String content;
+
+        public boolean isValid() {
+            return title != null && !title.isEmpty() && categories != null && !categories.isEmpty();
+        }
+    }
+
+    /**
+     * IN a real application you may want to use a DB, for this example we just store the posts in memory
+     */
+    public static class Model {
+        private int nextId = 1;
+        private final Map<Integer, Post> posts = new HashMap<>();
+
+        @Data
+        class Post {
+            private int id;
+            private String title;
+            private List categories;
+            private String content;
+        }
+
+        public int createPost(String title, String content, List categories) {
+            int id = nextId++;
+            Post post = new Post();
+            post.setId(id);
+            post.setTitle(title);
+            post.setContent(content);
+            post.setCategories(categories);
+            posts.put(id, post);
+            return id;
+        }
+
+        public List getAllPosts() {
+            return posts.keySet().stream().sorted().map(posts::get).collect(Collectors.toList());
+        }
+    }
+
+    private static String dataToJson(Object data) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            StringWriter sw = new StringWriter();
+            mapper.writeValue(sw, data);
+            return sw.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("IOException from a StringWriter?");
+        }
+    }
+
     public static void main(String[] args) {
-        get("/posts", (req, res) -> "Hello Sparkingly World!");
+        Model model = new Model();
+
+        // insert a post (using HTTP post method)
+        post("/posts", (request, response) -> {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                NewPostPayload creation = mapper.readValue(request.body(), NewPostPayload.class);
+                if (!creation.isValid()) {
+                    response.status(HTTP_BAD_REQUEST);
+                    return "";
+                }
+                int id = model.createPost(creation.getTitle(), creation.getContent(), creation.getCategories());
+                response.status(200);
+                response.type("application/json");
+                return id;
+            } catch (JsonParseException jpe) {
+                response.status(HTTP_BAD_REQUEST);
+                return "";
+            }
+        });
+
+        // get all post (using HTTP get method)
+        get("/posts", (request, response) -> {
+            response.status(200);
+            response.type("application/json");
+            return dataToJson(model.getAllPosts());
+        });
     }
 }
